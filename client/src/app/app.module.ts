1fa6b918ef4863783c7e60a35ab3b661d48d9f6c
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-06-01 20:02:52 +0700

    check shared module is work

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultModule } from './layouts/default/default.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultComponent,
-    DashboardComponent,
-    DefaultComponent
+    // DefaultComponent,
+    // DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
