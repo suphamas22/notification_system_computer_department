674c79a05b17b7aa45614578a057a09c4fab17c6
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-06-01 22:38:45 +0700

    add flex layout in header

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-
 import { MatDividerModule } from "@angular/material/divider"
 import { MatToolbarModule } from "@angular/material/toolbar"
 import { MatIconModule } from "@angular/material/icon"
+import { MatButtonModule } from "@angular/material/button"
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -19,7 +20,9 @@ import { MatIconModule } from "@angular/material/icon"
     CommonModule,
     MatDividerModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   exports: [
     HeaderComponent,
